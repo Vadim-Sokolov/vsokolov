085a7ef2d15b0e70b48566d891e745fdcae41fef
vook777 <vadim.sokolov@gmail.com>
2018-02-09 09:23:48 +0200

    changed alignment

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -13,10 +13,8 @@ public class DummyBot {
      */
     public String answer(String question) {
         String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
-		
         if ("Привет, Бот".equals(question)) {
 			rsl = "Привет, умник.";                    
-        
 		} else if ("Пока.".equals(question)) { 
             rsl = "До скорой встречи.";
         }
--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -15,7 +15,7 @@ public class Point {
         this.y = y;
     }
 
-    public double distanceTo(Point that){
+    public double distanceTo(Point that) {
 
         double result = Math.sqrt(
                 Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
@@ -23,9 +23,9 @@ public class Point {
         return result;
     }
 
-    public static void main(String[] args){
-        Point a = new Point(0,1);
-        Point b = new Point(2,5);
+    public static void main(String[] args) {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
 
         System.out.println("x1 = " + a.x);
         System.out.println("y1 = " + a.y);
