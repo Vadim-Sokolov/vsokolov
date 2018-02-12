9c28eb0bde8619bd1809edcaf14508a1f1321f11
vook777 <vadim.sokolov@gmail.com>
2018-02-12 09:56:04 +0200

    updtate 4.2 - Factorial

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -14,11 +14,8 @@ public class Factorial {
 	*/
 	public int calc(int n) {
 		int result = 1;
-		
-		if (n != 0) { 
-			for (int i = 1; i <= n; i++) {
-				result *= i;
-			}
+		for (int i = 1; i <= n; i++) {
+			result *= i;
 		}
 		return result;
 	}
