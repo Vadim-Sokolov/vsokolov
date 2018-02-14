9510ad07d9ca83ef1c41904d84ba46557006e8a4
vook777 <vadim.sokolov@gmail.com>
2018-02-14 11:33:01 +0200

    5.4. Двухмерный массив. Таблица умножения. [#33491]

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * @author vsokolov
+ * @version $Id$
+ * @since 0.1
+ */
+public class Matrix {
+	/**
+	* Method creates a matrix of given size and fills it
+	* with integers as a multiplication table. Returns the result.
+	* @param int size - size of matrix
+	* @return array
+	*/
+	public int[][] multiple(int size) {
+		
+		int[][] result = new int[size][size];		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				result[i][j] = (i + 1) * (j + 1);
+			}
+		}
+		return result;
+	}
+}
+				
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author vsokolov
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixTest {
+	
+	@Test
+	public void reverseOddArray() {
+		
+		Matrix mats = new Matrix();
+		int[][] expected = new int[][] {{1, 2, 3}, {2, 4, 6}, {3, 6, 9}};
+		int[][] result = mats.multiple(3);
+		assertThat(result, is(expected));
+	}
+}
