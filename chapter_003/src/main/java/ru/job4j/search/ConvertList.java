51cf3dfdb55fe2f93dcb094357c8d65919f8e8a9
vook777 <vadim.sokolov@gmail.com>
2018-03-17 11:17:17 +0200

    3. Конвертация двумерного массива в ArrayList и наоборот [#10035]

---
+++ chapter_003/src/main/java/ru/job4j/search/ConvertList.java
@@ -0,0 +1,54 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author vsokolov
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConvertList {
+
+    /**
+     * Method takes a two dimentional array of int,
+     * and converts them to a List<Integer>
+     * @param array - array to be converted
+     * @return List<Integer>
+     */
+
+    public List<Integer> toList(int[][] array) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] arr : array) {
+            for (int a : arr) {
+                result.add(a);
+            }
+        }
+        return result;
+    }
+    /**
+     * Method takes a list of Integers, and a number of rows
+     * and converts them to a two dimentional array.
+     * Left over positions are filled with zeros.
+     * @param list - List to be converted
+     * @param rows - number of rows to be filled
+     * @return int[][]
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        int size = list.size();
+        int secondSize = size / rows;
+        secondSize = size % rows == 0 ? secondSize : secondSize + 1;
+        int[][] result = new int[rows][secondSize];
+        int position = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < secondSize; j++) {
+                if (position < size) {
+                    result[i][j] = list.get(position++);
+                } else {
+                    result[i][j] = 0;
+                }
+            }
+        }
+        return result;
+    }
+}
