e0d769beddb641476c208920e2221b5242930b8d
vook777 <vadim.sokolov@gmail.com>
2018-03-17 09:18:47 +0200

    update

---
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,27 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author vsokolov
+ * @version $Id$
+ * @since 0.1
+ */
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        return result;
+    }
+}
