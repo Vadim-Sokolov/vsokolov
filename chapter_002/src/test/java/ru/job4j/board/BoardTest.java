265dba86e07e4aaa728ecbb1ccb9177b8d7cdcd5
vook777 <vadim.sokolov@gmail.com>
2018-04-15 08:38:54 +0300

    update

--- chapter_002/src/test/java/ru/job4j/board/BoardTest.java
+++ chapter_002/src/test/java/ru/job4j/board/BoardTest.java
@@ -9,7 +9,7 @@ public class BoardTest {
     @Test
     public void addTest() {
         Board board = new Board();
-        Cell cell = new Cell(1,2);
+        Cell cell = new Cell(1, 2);
         Bishop bishop = new Bishop(cell);
         board.add(bishop);
         assertThat(board.getFigures()[0], is(bishop));
@@ -18,7 +18,7 @@ public class BoardTest {
     @Test
     public void moveTest() throws OccupiedWayException, ImpossibleMoveException, FigureNotFoundException {
         Board board = new Board();
-        Cell source = new Cell(1,2);
+        Cell source = new Cell(1, 2);
         Cell dest = new Cell(4, 5);
         Bishop bishop = new Bishop(source);
         board.add(bishop);
@@ -28,7 +28,7 @@ public class BoardTest {
     @Test
     public void isPathClear() {
         Board board = new Board();
-        Cell[] cells = {new Cell(1,2), new Cell(2,3), new Cell(3, 4)};
+        Cell[] cells = {new Cell(1, 2), new Cell(2, 3), new Cell(3, 4)};
         assertThat(board.isPathClear(cells), is(true));
     }
 
