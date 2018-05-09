05212f0844ff289cf90e183570c299b81dd11039
vook777 <vadim.sokolov@gmail.com>
2018-05-09 09:33:18 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#9539]

---
+++ chapter_004/src/main/java/ru/job4j/ArrayIterator.java
@@ -0,0 +1,39 @@
+package ru.job4j;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
+/**
+ * @author vsokolov
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayIterator implements Iterator<Integer> {
+
+    private int[][] ints;
+    private int indexRow = 0;
+    private int indexColumn = 0;
+
+    public ArrayIterator(int[][] ints) {
+        this.ints = ints;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return indexRow < ints.length;
+    }
+
+    @Override
+    public Integer next() throws NoSuchElementException {
+        if (ints.length == 0) {
+            throw new NoSuchElementException();
+        }
+        int result = ints[indexRow][indexColumn++];
+        if (indexColumn == ints[indexRow].length) {
+            indexRow++;
+            indexColumn = 0;
+        }
+        return result;
+    }
+}
