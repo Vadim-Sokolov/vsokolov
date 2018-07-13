b9ba1e0c3f0effccfa6675a5b0062ae2e9fd54ce
vook777 <vadim.sokolov@gmail.com>
2018-07-13 09:50:04 +0300

    1. Неблокирующий кеш [#4741]

--- chapter_005_multithreading/src/main/java/ru/job4j/nonblockingalgorithm/NonBlockingCache.java
+++ chapter_005_multithreading/src/main/java/ru/job4j/nonblockingalgorithm/NonBlockingCache.java
@@ -12,6 +12,31 @@ public class NonBlockingCache {
 
     private ConcurrentHashMap<Integer, Base> cache = new ConcurrentHashMap<>();
 
+    public synchronized void add(Base model) {
+        if (cache.contains(model.getId())) {
+            model.setVersion(model.getVersion() + 1);
+        }
+        cache.put(model.getId(), model);
+    }
+
+    public synchronized void update(Base model) {
+        if (cache.contains(model.getId())) {
+            Base target = cache.get(model.getId());
+            target.setName(model.getName());
+            target.setVersion(target.getVersion() + 1);
+        }
+    }
+    public synchronized void delete(Base model) {
+        if (cache.contains(model.getId())) {
+            cache.remove(model.getId());
+        }
+    }
+
+
+
+
+
+
     BiFunction<Base, Base, Base> b = new BiFunction<Base, Base, Base>() {
         @Override
         public Base apply(Base o, Base o2) {
@@ -21,8 +46,4 @@ public class NonBlockingCache {
             return null;
         }
     };
-
-    public void add(Base model) { }
-    public void update(Base model) { }
-    public void delete(Base model) { }
 }
